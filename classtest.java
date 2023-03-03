4ec5332371d1d4562fcab78ab3eb38d9fad723ef
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-03 21:23:07 +0530

    To remove conflict

---
+++ classtest.java
@@ -0,0 +1,12 @@
+package testproject;
+
+public class classtest {
+ public static void main(String[] args) {
+	System.out.println("Helloworld" + "Shital"); 
+}
+}
+class a {
+	public static void main(String[] args) {
+		System.out.println("automation");
+	}
+}
